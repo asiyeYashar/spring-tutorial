f8e9a6e45ba27e8bac074f38cb3a2efd643bbad0
username <asiya11reyim@gmail.com>
2024-06-10 03:03:40 +0300

    spring-11-Data-TableCreation module created

---
+++ spring-11-Data-TableCreation/src/test/java/com/cydeo/dataapplication/Spring11DataTableCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.dataapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring11DataTableCreationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
