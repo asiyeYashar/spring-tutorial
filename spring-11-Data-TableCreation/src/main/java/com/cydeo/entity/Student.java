29374c3dd48febebfb6b8dfa8793703eac12e6d8
username <asiya11reyim@gmail.com>
2024-06-10 05:38:17 +0300

    enum class created and @Enumerated annotation used

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,6 +1,9 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.Gender;
 import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "students")
@@ -8,7 +11,17 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "studentFirstName")
     private String firstName;
+    @Column(name = "studentLastName")
     private String lastName;
     private String email;
+    @Column(columnDefinition="DATE")
+    private LocalDate birthDate;
+    @Column(columnDefinition="TIME")
+    private LocalDate birthTime;
+    @Column(columnDefinition="TIMESTAMP")
+    private LocalDate birthDateTime;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
 }
