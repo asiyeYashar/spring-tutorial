e0655bf3759c0dfc8c3530cf140ef11409dd7d23
username <asiya11reyim@gmail.com>
2024-07-07 16:19:17 +0300

    Cinema Lab

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/Spring13DataCinemaLabApplication.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/Spring13DataCinemaLabApplication.java
@@ -1,9 +1,9 @@
 package com.cydeo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import javax.sql.DataSource;
+
+
 
 @SpringBootApplication
 public class Spring13DataCinemaLabApplication {
