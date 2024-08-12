06078cec8cbd9c1acbc790fdf5eea3d18599a717
asiye <asiya11reyim@gmail.com>
2024-08-12 23:47:33 +0300

    application properties updated

---
+++ spring-15-Data-TableCreation/src/test/java/com/cydeo/spring15datatablecreation/Spring15DataTableCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring15datatablecreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring15DataTableCreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
